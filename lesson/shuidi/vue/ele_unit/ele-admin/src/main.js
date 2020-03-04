06f13a956835e6de31bc0954967f7898732dba04
Jayhjj <2762319789@qq.com>
2020-03-04 18:51:08 +0800

    ele_unit

--- lesson/shuidi/vue/ele_unit/ele-admin/src/main.js
+++ lesson/shuidi/vue/ele_unit/ele-admin/src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
-Vue.use(ElementUI)
+Vue.use(ElementUI) //element组件就可以全局访问了
 new Vue({
     router,
     render: h => h(App)
