8c845d1249e836378fe811d2613f5942e95d20f2
jayhjj <2762319789@qq.com>
2020-04-21 16:22:57 +0800

    hooks

---
+++ study/shuidi/webpack/lemon_hooks/src/index.tsx
@@ -0,0 +1,10 @@
+// import './app.css';
+// console.log('welcome');
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import{HelloComponent} from './hello';
+ReactDOM.render(
+    const [name,setName] = React.useState('defaultUserName');
+    <HelloComponent/>,
+    document.getElementById('root')
+)
