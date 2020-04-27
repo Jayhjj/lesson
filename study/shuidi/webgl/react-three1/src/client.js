7f37dc9b3feae6c5db7d7bc3c095ef4124b3c236
jayhjj <2762319789@qq.com>
2020-04-27 09:22:00 +0800

    react-three

---
+++ study/shuidi/webgl/react-three1/src/client.js
@@ -0,0 +1,16 @@
+// console.log('jay');
+// var a = 1;
+// document.body.innerHTML = a;
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import GameContainer from './containers/GameContainer.js';
+import GameContainer1 from './containers/1GameContainer.js';
+ReactDOM .render(  
+    <GameContainer/>,
+    document.getElementById('game')
+
+)  
+  
+
+ 
+
